9206e27aed0539dab4c401e87f033e237431fbfd
Sagar Beeravelly <sagarrao.b@gmail.com>
2013-04-12 16:35:59 -0500

    s

--- Mvc4Practise/webApi/Controllers/HomeController.cs
+++ Mvc4Practise/webApi/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace webApi.Controllers
 {
     public class HomeController : Controller
     {
+        public const string variable1 = "1";
         public ActionResult Index()
         {
             return View();
